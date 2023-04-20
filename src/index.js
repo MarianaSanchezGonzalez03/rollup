e6ef88130c9196d5ef0cbceae0f582814e0d3939
MarianaSanchezGonzalez03 <mari98san33@gmail.com>
2023-04-19 18:38:03 -0600

    Preparación del proyecto

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const $body = document.querySelector("body");
+const API='https://api.escuelajs.co/api/v1/products/?offset=5&limit=10';
+import logo from './assets/logo.png';
+import './styles/styles.css';
